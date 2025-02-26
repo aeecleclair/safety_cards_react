a44c4cccdc7aabe9ba1d3db6e8f8f98b596ed90f
grosjeal <loralinegrosjean@etu.ec-lyon.fr>
2025-02-26 16:51:55 +0100

    Alcool ?

--- front/src/pages/Risques-addictifs/Alcool.jsx
+++ front/src/pages/Risques-addictifs/Alcool.jsx
@@ -18,8 +18,18 @@ const Alcool = () => {
         text="Il ne faut pas que j'oublie de modifier cette citation." 
         author="Loraline"
       />
-      </div>
       
+      <ImageTextPopup
+        image="./src/assets/feu.jpg"
+        title="La carte 6♠"
+        shortText="Prévention sur la banalisation de l'alcool en soirée"
+        longText="Ceci est une phrase qui vise à rallonger la  longueur du texte. L’image présente un paysage naturel captivant où la lumière douce du matin baigne l'ensemble de la scène. Au premier plan, une prairie verdoyante s'étend à perte de vue, avec des herbes hautes qui dansent lentement sous l'effet d’une brise légère. Les couleurs du sol, d’un vert tendre, se mélangent subtilement aux fleurs sauvages de toutes sortes : des marguerites blanches, des coquelicots rouges éclatants, et des boutons d’or jaunes, ajoutant une touche de couleur vive au tableau. Plus loin, un sentier sinueux se fraye un chemin à travers cette végétation, invitant à l’exploration.
+        Ceci est une phrase qui vise à rallonger la  longueur du texte. L’image présente un paysage naturel captivant où la lumière douce du matin baigne l'ensemble de la scène. Au premier plan, une prairie verdoyante s'étend à perte de vue, avec des herbes hautes qui dansent lentement sous l'effet d’une brise légère. Les couleurs du sol, d’un vert tendre, se mélangent subtilement aux fleurs sauvages de toutes sortes : des marguerites blanches, des coquelicots rouges éclatants, et des boutons d’or jaunes, ajoutant une touche de couleur vive au tableau. Plus loin, un sentier sinueux se fraye un chemin à travers cette végétation, invitant à l’exploration."
+        textButton="⤢ Agrandir la carte"
+      />
+
+      </div>
+
     );
   };
   
