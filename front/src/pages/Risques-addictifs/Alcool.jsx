00ba0c0e0fb4b4f0179977d3bbc7351b2299b6fb
grosjeal <loralinegrosjean@etu.ec-lyon.fr>
2025-02-26 16:51:34 +0100

    Page Alcool

--- front/src/pages/Risques-addictifs/Alcool.jsx
+++ front/src/pages/Risques-addictifs/Alcool.jsx
@@ -1,9 +1,25 @@
+import Quote from "../../components/Citation"; 
+import ImageTextPopup from "../../components/Cartes"; 
+import ListeNumerotee from "../../components/Listes";
+import ExternalLinkBlock from "../../components/Liens-ext";
+import Components from "../../components/Common";
+import ContactCard from "../../components/Contact";
+import React from "react";
+import "../../App.css";
+
+
 const Alcool = () => {
     return (
-      <div>
-        <h1>Alcool</h1>
+      <div className="page">
+        <h1 className="titre-page">Alcool</h1>
         <p>Voici la page Alcool</p>
+        
+      <Quote 
+        text="Il ne faut pas que j'oublie de modifier cette citation." 
+        author="Loraline"
+      />
       </div>
+      
     );
   };
   
