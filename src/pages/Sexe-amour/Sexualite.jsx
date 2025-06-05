6ffe05526f401fb37e03fcd9bce20412b0fab58f
Adrien G <adrien.gaubert@etu.ec-lyon.fr>
2025-06-05 11:02:07 +0200

    v2.0.1 - Correction d'un bug

--- src/pages/Sexe-amour/Sexualite.jsx
+++ src/pages/Sexe-amour/Sexualite.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../../App.css";
-import "./sexualite.css";
+import "./Sexualite.css";
 
 import Quote from "../../components/Citation";
 import ImageTextPopup from "../../components/Cartes";
