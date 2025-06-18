c73e6e561e7d5aefbb945d0d0e8a077b46a4e12a
surjis chloé <chloe.surjis@etu.ec-lyon>
2025-06-18 17:40:54 +0200

    test Chloé

--- src/pages/Connaissance-soi/Connaissance-de-soi-main.jsx
+++ src/pages/Connaissance-soi/Connaissance-de-soi-main.jsx
@@ -2,7 +2,7 @@ const Connaissance_soi = () => {
     return (
       <div>
         <h1>Connaissance de soi</h1>
-        <p>Voici la page Connaissance de soi</p>
+        <p>Voici la page Connaissance de soi   </p>
       </div>
     );
   };
