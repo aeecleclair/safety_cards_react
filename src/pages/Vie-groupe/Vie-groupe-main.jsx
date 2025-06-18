e3bd575639944bd58e102b883e1c93f15b360373
grosjeal <loralinegrosjean@etu.ec-lyon.fr>
2025-06-18 17:22:27 +0200

    test loraline

--- src/pages/Vie-groupe/Vie-groupe-main.jsx
+++ src/pages/Vie-groupe/Vie-groupe-main.jsx
@@ -2,7 +2,7 @@ const Vie_groupe = () => {
     return (
       <div>
         <h1>Vie de groupe</h1>
-        <p>Voici la page Vie de groupe</p>
+        <p>Voici la page Vie de groupe   </p>
       </div>
     );
   };
