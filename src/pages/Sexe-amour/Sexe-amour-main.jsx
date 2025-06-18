09c2152e9b263c6e3672efbe70d48dd0c74bc808
Rayan Oussmou <rayan@host0599.wifi.ec-lyon.fr>
2025-06-18 14:13:29 +0200

    test Rayan

--- src/pages/Sexe-amour/Sexe-amour-main.jsx
+++ src/pages/Sexe-amour/Sexe-amour-main.jsx
@@ -2,7 +2,7 @@ const Sexe_amour = () => {
     return (
       <div>
         <h1>Sexualité et vie amoureuse </h1>
-        <p>Voici la page Sexualité et vie amoureuse</p>
+        <p>Voici la page Sexualité et vie amoureuse aaa</p>
       </div>
     );
   };
