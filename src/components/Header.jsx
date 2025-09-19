bc478da05f30feda7f698037fa79caacea4dc2bd
Adrien G <adrien.gaubert@etu.ec-lyon.fr>
2025-09-20 00:36:49 +0200

    Page relations de couple, modifications autres mineures
    Replaced and added several image assets, updated references to Peer Care logo, and improved the 'Relations de couple' page with expanded content on violence, sexuality, and communication. Also updated styles and fixed navigation/menu item activation for the couple section.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -46,7 +46,7 @@ const menuItems = [
       { title: "⚠️ IST", link: "/pratiques_sex", inactive:true },
       { title: "♀️ IVG", link: "/ivg", inactive:true },
       { title: "🏳️‍🌈 Orientation sexuelle", link: "/sex_orient", inactive:true },
-      { title: "💑 Relations de couple", link: "/couple", inactive:true },
+      { title: "💑 Relations de couple", link: "/couple" },
       { title: "⛔ Revenge porn", link: "/revenge" },
       { title: "😘 Sexualité", link: "/sex", inactive:true },
     ],
